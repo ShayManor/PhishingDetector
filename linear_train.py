7d7fd2f89532fa076dbe388e79cd2419f4e08c72
shay <shay.manor@gmail.com>
2025-04-20 20:06:08 -0400

    Linear 61% accuracy

--- linear_train.py
+++ linear_train.py
@@ -1,28 +1,26 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 scalar = MinMaxScaler()
 df = pd.read_csv('email_phishing_data.csv')
-df['num_words_norm'] = scalar.fit_transform(df[['num_words']])
-df['num_unique_words_norm'] = scalar.fit_transform(df[['num_unique_words']])
-df['num_stopwords_norm'] = scalar.fit_transform(df[['num_words']])
-df['num_links_norm'] = scalar.fit_transform(df[['num_links']])
-df['num_unique_domains_norm'] = scalar.fit_transform(df[['num_unique_domains']])
-df['num_email_addresses_norm'] = scalar.fit_transform(df[['num_email_addresses']])
-df['num_spelling_errors_norm'] = scalar.fit_transform(df[['num_spelling_errors']])
-df['num_urgent_keywords_norm'] = scalar.fit_transform(df[['num_urgent_keywords']])
-X = df.drop(['label', 'num_words', 'num_unique_words', 'num_stopwords', 'num_links', 'num_unique_domains', 'num_email_addresses', 'num_spelling_errors', 'num_urgent_keywords'], axis=1).values
+features = [
+  'num_words','num_unique_words','num_stopwords','num_links',
+  'num_unique_domains','num_email_addresses','num_spelling_errors','num_urgent_keywords'
+]
+df[features] = MinMaxScaler().fit_transform(df[features])
+X = df.drop(['label'], axis=1).values
 y = df['label'].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
-model = LogisticRegression(multi_class='ovr', max_iter=10000)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+model_cv = LogisticRegressionCV(cv=5, max_iter=5000)
+model_cv.fit(X_train, y_train)
+y_pred = model_cv.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
+print(pd.Series(model_cv.coef_[0], index=features).sort_values())
 from collections import Counter
 print(Counter(y_train), Counter(y_test))
 print("Baseline:", max(Counter(y_test).values())/len(y_test))
--- split_data.py
+++ split_data.py
@@ -2,27 +2,32 @@ import random
 
 with open('email_phishing_data2.csv', 'r') as f:
     lines = f.readlines()
-res = []
-for line in lines:
-    if line.split(',')[8].count('1') != 0:
-        res.append(line)
-    elif random.random() <= 0.015:
-        res.append(line)
-with open('email_phishing_data.csv', 'w') as f:
-    f.writelines(res)
 
-# test = []
-# train = []
-#
-# for line in lines:
-#     if random.random() <= 0.15:
-#         test.append(line)
-#     else:
-#         train.append(line)
-# with open('test.csv', 'w') as f:
-#     f.writelines(test)
-# with open('train.csv', 'w') as f:
-#     f.writelines(train)
-# print(f'Test: {len(test)}')
-# print(f'Train: {len(train)}')
-# print(f'Split: {100.0 * len(test) / len(train)}%')
+
+def clean():
+    res = []
+    for line in lines:
+        if line.split(',')[8].count('1') != 0:
+            res.append(line)
+        elif random.random() <= 0.015:
+            res.append(line)
+    with open('email_phishing_data.csv', 'w') as f:
+        f.writelines(res)
+
+def split():
+    test = []
+    train = []
+
+    for line in lines:
+        if random.random() <= 0.15:
+            test.append(line)
+        else:
+            train.append(line)
+    with open('test.csv', 'w') as f:
+        f.writelines(test)
+    with open('train.csv', 'w') as f:
+        f.writelines(train)
+    print(f'Test: {len(test)}')
+    print(f'Train: {len(train)}')
+    print(f'Split: {100.0 * len(test) / len(train)}%')
+split()
