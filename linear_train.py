09929b56c92f916d797735b13660bf3673ee9696
shay <shay.manor@gmail.com>
2025-04-21 21:43:44 -0400

    Added linear

--- linear_train.py
+++ linear_train.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from collections import Counter
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -14,13 +15,12 @@ df[features] = MinMaxScaler().fit_transform(df[features])
 X = df.drop(['label'], axis=1).values
 y = df['label'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
-model_cv = LogisticRegressionCV(cv=5, max_iter=5000)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, shuffle=True)
+model_cv = LogisticRegressionCV(cv=10, max_iter=50000)
 model_cv.fit(X_train, y_train)
 y_pred = model_cv.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 print(pd.Series(model_cv.coef_[0], index=features).sort_values())
-from collections import Counter
 print(Counter(y_train), Counter(y_test))
 print("Baseline:", max(Counter(y_test).values())/len(y_test))
